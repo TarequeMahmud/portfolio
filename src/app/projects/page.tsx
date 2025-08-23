35c87ac2e076a7ed80875e77a22835ce93954e8b
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-08-23 13:44:02 +0600

    feat: optimize project images. refactor project and project card component.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,13 +5,13 @@ import projects from "@/data/projects.json";
 
 const Page = () => {
   return (
-    <div className="w-full min-h-[600px] p-5 flex flex-col items-center">
+    <div className="w-full min-h-[600px] p-5 flex flex-col items-center m-auto">
       <h1 className="mt-5 mb-2 h-10 text-3xl font-semibold">
         PROJECT EXPERIENCE
       </h1>
       <hr className="w-full border-gray-400" />
 
-      <div className="my-12 w-full flex flex-wrap gap-8 justify-center">
+      <div className="my-12 w-full grid grid-cols-1 md:grid-cols-2 gap-8 justify-items-center">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,37 +7,40 @@ type Project = {
   image: string;
   description: string;
   techStack: string;
+  gradient: string;
 };
 
 const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="w-[45%] max-w-2xl bg-gradient-to-r from-[#176c94] to-[#092735] rounded-2xl border-4 border-cyan-900 shadow-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-transform">
+    <div
+      className={`w-[90%] max-w-2xl bg-gradient-to-r ${project.gradient} 
+      rounded-2xl border-4 border-[#ffffffde] shadow-lg overflow-hidden 
+      cursor-pointer hover:scale-[1.02] transition-transform`}
+    >
       {/* Image */}
-      <div className="w-full h-64 relative">
+      <div className="w-full aspect-[16/9] relative">
         <Image
           src={project.image}
           alt={`${project.title} preview`}
-          className="object-center"
+          className="object-cover rounded-t-2xl"
           fill
+          sizes="(max-width: 768px) 100vw, 50vw"
+          priority={project.id === 1}
         />
       </div>
 
       {/* Content */}
       <div className="p-6 space-y-4">
-        {/* Tag */}
         <span className="inline-block px-3 py-1 text-sm font-medium bg-black/60 text-white rounded-md">
           {project.tag}
         </span>
 
-        {/* Title */}
         <h2 className="text-2xl font-bold text-white">{project.title}</h2>
 
-        {/* Description */}
         <p className="text-gray-200 text-sm leading-relaxed">
           {project.description}
         </p>
 
-        {/* Tech Stack */}
         <p className="text-gray-300 text-xs italic">{project.techStack}</p>
       </div>
     </div>
