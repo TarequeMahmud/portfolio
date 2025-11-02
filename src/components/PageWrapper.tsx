d3d3da03eab2949c87f23a4fd35420a5ec4f2b53
Tareque Mahmud <tarequemahmud313@gamil.com>
2025-11-02 21:43:26 +0600

    feat: make welcome section and its animation responsive.

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -13,14 +13,26 @@ const PageWrapper: React.FC<PageWrapperProps> = ({
   subtitle,
   minHeight = "min-h-[600px]",
 }) => {
+  // accept either tailwind minHeight class or fallback
+  const minHClass = minHeight || "min-h-[600px]";
+
   return (
     <div
-      className={`w-full ${minHeight} p-5 flex flex-col items-center mx-auto my-2 gap-6`}
+      className={`
+        w-[95%] sm:w-[92%] md:w-[90%] lg:w-[85%] ${minHClass} 
+        p-4 sm:p-6 md:p-8 flex flex-col items-center mx-auto my-2 gap-6
+      `}
     >
       {title && (
         <>
-          <h1 className="mt-5 mb-2 text-3xl font-semibold">{title}</h1>
-          {subtitle && <p className="mt-[-20px] mb-[-10px] p-0">{subtitle}</p>}
+          <h1 className="mt-5 mb-2 text-2xl sm:text-3xl md:text-3xl font-semibold text-center">
+            {title}
+          </h1>
+          {subtitle && (
+            <p className="mt-0 mb-0 text-sm sm:text-base text-center text-gray-300">
+              {subtitle}
+            </p>
+          )}
           <hr className="w-full border-gray-400" />
         </>
       )}
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,14 +1,14 @@
 const Welcome = () => {
   return (
-    <div className="relative overflow-visible w-full h-[600px] sm:h-[500px] md:h-[600px]">
+    <div className="relative overflow-visible w-full h-[200px] sm:h-[500px] md:h-[600px]">
       <div className="absolute w-[80%] sm:w-[60%] md:w-[50%] flex flex-col items-center justify-center animate-moveBounce">
-        <h1 className="text-3xl sm:text-4xl md:text-[2.5rem] font-medium animate-colorShift transition-colors duration-1000 ease-in-out text-center">
+        <h1 className="text-md sm:text-4xl md:text-[2.5rem] font-medium animate-colorShift transition-colors duration-1000 ease-in-out text-center">
           Hello, I&#39;m Tareque Mahmud
         </h1>
-        <p className="text-xl sm:text-2xl font-normal animate-colorShift transition-colors duration-1000 ease-in-out text-center">
+        <p className="text-xs sm:text-2xl font-normal animate-colorShift transition-colors duration-1000 ease-in-out text-center">
           I build full stack web applications.
           <br />
-          <span className="text-[1.1rem]">
+          <span className="text-xs sm:text-[1.1rem]">
             Next.js, React | Nest, Express | Django, FastAPI | SQL, MongoDB |
             Docker, AWS
           </span>
